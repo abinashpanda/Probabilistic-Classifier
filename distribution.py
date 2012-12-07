e2843d74c183421cb25d4f12fe0b09809c5b5070
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2012-12-07 23:31:56 +0530

    first commit

--- distribution.py
+++ distribution.py
@@ -5,27 +5,58 @@ import numpy as np
 from scipy import integrate
 from scipy import optimize
 import pylab
-def asymetricGaussianDensity(x,mean=0,sigma2 = 1,r = 1):
+import csv
+def getInputData(filename):
+	fileobject = open(filename,'rb')
+	csvfile = csv.reader(fileobject)
+	class1_data = []
+	class2_data = []
+	for row in csvfile:
+		class_data = int(row[0])
+		if class_data == 1:
+			class1_data.append(float(row[1]))
+		else :
+			class2_data.append(float(row[1]))
+	class1_data = np.array(class1_data)
+	class2_data = np.array(class2_data)
+	fileobject.close()	
+	return class1_data,class2_data
+def computeProb(data,n1,mean_class1,sigma_class1,n2,mean_class2,sigma_class2,r_class2):
+	prob_v_c1 = distribution.probSymGaussian(data,mean_class1,sigma_class1)
+	prob_v_c2 = distribution.probAsymGaussian(data,mean_class2,sigma_class2,r_class2)
+	prob1 = (n1*prob_v_c1)/((n1*prob_v_c1) + (n2*prob_v_c2))
+	prob2 = (n2*prob_v_c2)/((n1*prob_v_c1) + (n2*prob_v_c2))
+	prob1 = round(prob1,2)
+	prob2 = round(prob2,2)	
+	return prob1,prob2
+def classPrediction(prob1,prob2):
+	if prob1 > (prob2): 
+		return 1
+	else :
+		return 2	
+pi = 6.2832
+def asymetricGaussianDensity(x,mean=0,sigma = 1,r = 1):
+	sigma_squared = math.pow(sigma,2)
+	scaling_factor = (2.0 / math.sqrt(pi)) * (1.0 / (sigma * (r+1)))
 	x_squared = math.pow((x-mean),2)
 	if x > mean :	
-		x_squared = -x_squared/(2*sigma2)
+		x_squared = -x_squared/(sigma_squared)
+		y = scaling_factor * math.exp(0.5 * x_squared)
 	else : 
-		x_squared = -x_squared/(2*sigma2*r*r)	
-	y = math.exp(x_squared)
-	y = (2*y)/(((math.sqrt(6.28*sigma2))*(r+1)))
+		x_squared = -x_squared/(sigma_squared*math.pow(r,2))	
+		y = scaling_factor * math.exp(0.5 * x_squared)
+	return y	
+def symetricGaussianDensity(x,mean=0,sigma = 1):
+	scaling_factor = 1.0/(math.sqrt(pi)*sigma)
+	x_squared = math.pow((x-mean)/sigma,2)
+	y = scaling_factor*math.exp(-0.5*x_squared)	
 	return y
-def symetricGaussianDensity(x,mean=0,sigma2 = 1):
-	x_squared = math.pow((x-mean),2)
-	x_squared = -x_squared/(2*sigma2)	
-	y = math.exp(x_squared)
-	y = y/(math.sqrt(6.28*sigma2))
-	return y
-def symetricalNormalDistribution(size,mean = 0, sigma2 = 1):
+def symetricalNormalDistribution(size,mean = 0, sigma = 1):
 	y = []
-	x = np.arange(round((mean - (3*math.sqrt(sigma2))),3),round((mean + (3*math.sqrt(sigma2))),3),0.001)
+	x = np.arange(round((mean - (5*sigma)),3),round((mean + (5*sigma)),3),0.001)
 	y_cum = []
 	for val in x:
-		y_cum.append(integrate.quad(symetricGaussianDensity,round((mean - (3*math.sqrt(sigma2))),3),float(val),args = (mean,sigma2)))
+		y_cum.append(integrate.quad(symetricGaussianDensity,round((mean - (5*sigma)),3),float(val),args = (mean,sigma)))
 	y_cum = np.array(y_cum)	
 	diction_data = {}
 	for val in range(len(x)):
@@ -39,36 +70,36 @@ def symetricalNormalDistribution(size,mean = 0, sigma2 = 1):
 			y.append(diction_data[uniformDistribution[val]])
 	y = np.array(y)	
 	return y
-def asymetricalNormalDistribution(size,mean = 0,sigma2 = 1, r = 1):
+def asymetricalNormalDistribution(size,mean = 0,sigma = 1, r = 1):
 	y = []
-	x = np.arange(round((mean - (3*math.sqrt(sigma2))),3),round((mean + (3*math.sqrt(sigma2))),3),0.001)
+	x = np.arange(round((mean - (5*sigma)),3),round((mean + (5*sigma)),3),0.001)
 	y_cum = []
 	for val in x:
-		y_cum.append(integrate.quad(asymetricGaussianDensity,round((mean - (3*math.sqrt(sigma2))),3),float(val),args = (mean,sigma2,r)))
+		y_cum.append(integrate.quad(asymetricGaussianDensity,round((mean - (5*sigma)),3),float(val),args = (mean,sigma,r)))
 	y_cum = np.array(y_cum)	
 	diction_data = {}
 	for val in range(len(x)):
 		diction_data.update({round(float(y_cum[val,0]),3):round(float(x[val]),3)})
 	uniformDistribution = []
 	for val in range(size):
-		uniformDistribution.append(round(random.random(),2))
+		uniformDistribution.append(round(random.random(),3))
 	for val in range(len(uniformDistribution)):
 		comp = diction_data.has_key(uniformDistribution[val])
 		if comp :
 			y.append(diction_data[uniformDistribution[val]])	
 	y = np.array(y)	
-	return y	
-def probAsymGaussian(x,mean,sigma2,r):
-	return integrate.quad(asymetricGaussianDensity,round((mean - (3*math.sqrt(sigma2))),3),x,args = (mean,sigma2,r))[0]
-def probSymGaussian(x,mean,sigma2):
-	return integrate.quad(symetricGaussianDensity,round((mean - (3*math.sqrt(sigma2))),3),x,args = (mean,sigma2))[0]
-def gauss_likelyhood(mean,sigma2,y):
+	return y
+def probAsymGaussian(x,mean,sigma,r):
+	return integrate.quad(asymetricGaussianDensity,round((mean - (5*sigma)),3),x,args = (mean,sigma,r))[0]
+def probSymGaussian(x,mean,sigma):
+	return integrate.quad(symetricGaussianDensity,round((mean - (5*sigma)),3),x,args = (mean,sigma))[0]
+def gauss_likelyhood(mean,sigma,y):
 	arr = np.power((y-mean),2)
 	arr_sum = arr.sum()
-	arr_sum = arr_sum/(2*sigma2)
+	arr_sum = arr_sum/(2*sigma*sigma)
 	arr_sum = arr_sum
 	r = len(y)
-	ret = r*((math.log(6.28)/2) + (math.log(sigma2)/2))
+	ret = r*((math.log(pi)/2) + (math.log(sigma)))
 	ret = ret + arr_sum
 	ret = ret
 	return ret
@@ -76,6 +107,8 @@ def estimateGaussianParams(inputdata):
 	sigma,mean = 1,0 
 	mean_estimated = optimize.fmin(lambda m: gauss_likelyhood(m,sigma,inputdata),mean)
 	sigma_estimated = optimize.fmin(lambda s: gauss_likelyhood(mean_estimated,s,inputdata),sigma)
+	mean_estimated = round(mean_estimated,2)
+	sigma_estimated = round(sigma_estimated,2)
 	return mean_estimated,sigma_estimated	
 def sgn_arr(y,thresh):
 	y_sgn = []
@@ -87,33 +120,35 @@ def sgn_arr(y,thresh):
 	y_sgn = np.array(y_sgn)
 	y_invert = (1-y_sgn)
 	return y_sgn,y_invert
-def asymgauss_likelyhood(mean,sigma2,r,inputdata):
+def asymgauss_likelyhood(mean,sigma,r,inputdata):
 	arr = np.power((inputdata-mean),2)
 	y_sgn,y_invert = sgn_arr(inputdata,mean)
-	arr2 = ((y_sgn*arr)/(2*sigma2)) + ((y_invert*arr)/(2*sigma2*r*r))
+	arr2 = ((y_sgn*arr)/(2*sigma*sigma)) + ((y_invert*arr)/(2*sigma*sigma*r*r))
 	arr_sum = arr2.sum()
 	length = len(inputdata)
-	ret = ((math.log(6.28)/2) + (math.log(sigma2)/2)+ math.log(r+1) - math.log(2))
+	ret = ((math.log(pi)/2) + (math.log(sigma))+ math.log(r+1) - math.log(2))
 	ret = ret*length	
 	ret = ret + arr_sum
 	ret = ret
 	return ret
 def estimateAsymGaussianParams(inputdata):
 	mean_estimated = 0
-	sigma2_estimated = 1
+	sigma_estimated = 1
 	r_estimated = 1
 	for val in range(50): 
-		mean_estimated = optimize.fmin(lambda m: asymgauss_likelyhood(m,sigma2_estimated,r_estimated,inputdata),mean_estimated)
-		sigma2_estimated = optimize.fmin(lambda s : asymgauss_likelyhood(mean_estimated,s,r_estimated,inputdata),sigma2_estimated)
-		r_estimated = optimize.fmin(lambda r : asymgauss_likelyhood(mean_estimated,sigma2_estimated,r,inputdata),r_estimated)
+		mean_estimated = optimize.fmin(lambda m: asymgauss_likelyhood(m,sigma_estimated,r_estimated,inputdata),mean_estimated)
+		sigma_estimated = optimize.fmin(lambda s : asymgauss_likelyhood(mean_estimated,s,r_estimated,inputdata),sigma_estimated)
+		r_estimated = optimize.fmin(lambda r : asymgauss_likelyhood(mean_estimated,sigma_estimated,r,inputdata),r_estimated)
 	mean = round(mean_estimated,2)
-	sigma2 = round(sigma2_estimated,2)
+	sigma = round(sigma_estimated,2)
 	r = round(r_estimated,2)
-	return mean,sigma2,r
+	return mean,sigma,r
 if __name__ == '__main__':
 	inputdata = asymetricalNormalDistribution(100,0,2,0.5)
-	mean,sigma2,r = estimateAsymGaussianParams(inputdata)	
-	print mean,sigma2,r	
-	inputdata2 = symetricalNormalDistribution(15,-3,1.5)
-	mean,sigma2 = estimateGaussianParams(inputdata2)
-	print mean,sigma2
+	mean,sigma,r = estimateAsymGaussianParams(inputdata)	
+	print mean,sigma,r	
+	inputdata2 = symetricalNormalDistribution(100,-3,1.5)
+	mean,sigma = estimateGaussianParams(inputdata2)
+	print mean,sigma
+	print inputdata.size
+	print inputdata2.size
